89f43e2c32c31f076090f7a454c2b11083ea282a
Mike Sul <mike.sul@foundries.io>
2023-12-12 12:13:53 +0100

    Add shortlist flag to the run command
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/composectl/cmd/run.go
+++ cmd/composectl/cmd/run.go
@@ -7,18 +7,23 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
+)
+
+var (
+	runCmd = &cobra.Command{
+		Use:   "run",
+		Short: "",
+		Long:  ``,
+		Args:  cobra.NoArgs,
+		Run:   runApps,
+	}
+	runAppShortlist *string
 )
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-}
-
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "",
-	Long:  ``,
-	Args:  cobra.NoArgs,
-	Run:   runApps,
+	runAppShortlist = runCmd.Flags().String("apps", "", "Comma separated list of apps to run; all installed apps are started if not defined")
 }
 
 func runApps(cmd *cobra.Command, args []string) {
@@ -26,7 +31,19 @@ func runApps(cmd *cobra.Command, args []string) {
 	DieNotNil(err)
 	apps, err := cs.ListApps(cmd.Context())
 	DieNotNil(err)
+	var appShortlist map[string]bool
+	if len(*runAppShortlist) > 0 {
+		appShortlist = make(map[string]bool)
+		for _, a := range strings.Split(*runAppShortlist, ",") {
+			appShortlist[a] = true
+		}
+	}
+
 	for _, a := range apps {
+		if appShortlist != nil && !appShortlist[a.Name] {
+			fmt.Printf("Skipping starting %s since it is not in the specfified shortlist\n", a.Name)
+			continue
+		}
 		fmt.Printf("Starting %s --> %s\n", a.Name, a.Spec.String())
 		cmd := exec.Command("docker", "compose", "up", "-d", "--remove-orphans")
 		cmd.Dir = path.Join(config.ComposeRoot, a.Name)
