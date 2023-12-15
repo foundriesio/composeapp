9cd207fc8e3c085095f4e74a0d76bc3a237dcdba
Mike Sul <mike.sul@foundries.io>
2023-12-15 18:04:45 +0100

    auth: Add oauth support
    Support the oauth2 authentication schema required if an auth credential
    helper returns an identity token to access registry.
    This "identity" token plays a role of the `refresh_token` in
    the oauth schema.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/compose/auth.go
+++ pkg/compose/auth.go
@@ -1,6 +1,7 @@
 package compose
 
 import (
+	"fmt"
 	"github.com/containerd/containerd/remotes/docker"
 	"github.com/docker/cli/cli/config/configfile"
 )
@@ -15,10 +16,23 @@ type (
 
 func getAuthCreds(cfg *configfile.ConfigFile) authCredsFunc {
 	return func(host string) (string, string, error) {
-		auth, err := cfg.GetAuthConfig(host)
+		creds, err := cfg.GetAllCredentials()
 		if err != nil {
 			return "", "", err
 		}
-		return auth.Username, auth.Password, nil
+		auth, ok := creds[host]
+		if !ok {
+			// no auth config found, return no error to try anonymous access
+			return "", "", nil
+		}
+		if len(auth.Username) > 0 && len(auth.Password) > 0 {
+			// basic auth
+			return auth.Username, auth.Password, nil
+		}
+		if len(auth.IdentityToken) > 0 {
+			// oauth auth
+			return "", auth.IdentityToken, nil
+		}
+		return "", "", fmt.Errorf("neither user creds nor identity token is obtained for the given host: %s", host)
 	}
 }
