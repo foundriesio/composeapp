805fbfde63f6e57e33c153dd91783de473b3a435
Mike Sul <mike.sul@foundries.io>
2024-05-16 15:19:43 +0200

    list: Add json output
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/composectl/cmd/list.go
+++ cmd/composectl/cmd/list.go
@@ -1,29 +1,60 @@
 package composectl
 
 import (
+	"encoding/json"
 	"fmt"
 	v1 "github.com/foundriesio/composeapp/pkg/compose/v1"
 	"github.com/spf13/cobra"
 )
 
+var listCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "list apps found in the store",
+	Long:  ``,
+	Args:  cobra.NoArgs,
+}
+
+type (
+	listOptions struct {
+		Format string
+	}
+	appJsonOutput struct {
+		Name string `json:"name"`
+		URI  string `json:"uri"`
+	}
+)
+
 func init() {
+	opts := listOptions{}
+	listCmd.Flags().StringVar(&opts.Format, "format", "plain", "Format the output. Values: [plain | json]")
+	listCmd.Run = func(cmd *cobra.Command, args []string) {
+		if opts.Format != "plain" && opts.Format != "json" {
+			DieNotNil(fmt.Errorf("invalid value of `--format` option: %s", opts.Format))
+		}
+		listApps(cmd, args, &opts)
+	}
 	rootCmd.AddCommand(listCmd)
 }
 
-var listCmd = &cobra.Command{
-	Use:   "ls",
-	Short: "",
-	Long:  ``,
-	Args:  cobra.NoArgs,
-	Run:   listApps,
-}
-
-func listApps(cmd *cobra.Command, args []string) {
+func listApps(cmd *cobra.Command, args []string, opts *listOptions) {
 	cs, err := v1.NewAppStore(config.StoreRoot, config.Platform)
 	DieNotNil(err)
 	apps, err := cs.ListApps(cmd.Context())
 	DieNotNil(err)
-	for _, a := range apps {
-		fmt.Printf("%s -> %s\n", a.Name, a.String())
+	if opts.Format == "json" {
+		var lsOutput []appJsonOutput
+		for _, app := range apps {
+			lsOutput = append(lsOutput, appJsonOutput{
+				Name: app.Name,
+				URI:  app.String(),
+			})
+		}
+		if b, err := json.MarshalIndent(lsOutput, "", "  "); err == nil {
+			fmt.Println(string(b))
+		}
+	} else {
+		for _, a := range apps {
+			fmt.Printf("%s -> %s\n", a.Name, a.String())
+		}
 	}
 }
