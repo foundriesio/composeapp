061afe0d007817d0d52e8bf33b5c2a67c6523d68
Mike Sul <mike.sul@foundries.io>
2024-10-04 15:04:42 +0200

    test: Add and use utility to check error
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- test/fixtures/layers_meta.go
+++ test/fixtures/layers_meta.go
@@ -14,27 +14,15 @@ func GenerateLayersMetaFile(t *testing.T, appsRootDir string) string {
 	c := exec.Command("python", os.Getenv("LAYERS_SIZE_SCRIPT"), "--apps-root",
 		appsRootDir, "-o", layerSizesFile)
 	output, err := c.CombinedOutput()
-	if err != nil {
-		t.Fatalf("failed to run the script that calculates app layer sizes: %s\n", output)
-	}
+	checkf(t, err, "failed to run command to gather app layer sizes: %s", string(output))
 	b, err := os.ReadFile(layerSizesFile)
-	if err != nil {
-		t.Fatalf("failed to read file witj app layer sizes: %s\n", err)
-	}
+	check(t, err)
 	var ls map[string]interface{}
-	if err := json.Unmarshal(b, &ls); err != nil {
-		t.Fatalf("failed to unmarshal app layer sizes: %s\n", err)
-	}
-	lsPerArch := map[string]interface{}{
-		"amd64": ls,
-	}
+	check(t, json.Unmarshal(b, &ls))
+	lsPerArch := map[string]interface{}{"amd64": ls}
 	b, err = json.Marshal(lsPerArch)
-	if err != nil {
-		t.Fatalf("failed to marshal app layer sizes per architecure: %s\n", err)
-	}
+	check(t, err)
 	layersMetaFile := filepath.Join(tmpDir, "layers_meta_per_arch.json")
-	if err := os.WriteFile(layersMetaFile, b, 0x644); err != nil {
-		t.Fatalf("failed to write file: %s\n", err)
-	}
+	check(t, os.WriteFile(layersMetaFile, b, 0x644))
 	return layersMetaFile
 }
