92122b1df315d3d910162ce84ed0cac409a25869
Mike Sul <mike.sul@foundries.io>
2025-03-20 12:34:52 +0100

    progress: Add component to report task progress
    Add a new template class aimed to report and handle a task execution
    progress asynchronously.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

---
+++ internal/progress/reporter_test.go
@@ -0,0 +1,57 @@
+package progress
+
+import (
+	"testing"
+)
+
+func TestReporter_NoBlocking(t *testing.T) {
+	maxProgress := 5
+	progressReporter := NewReporter[int](maxProgress + 1)
+	expectedProgress := 0
+
+	defer func() {
+		progressReporter.Stop(true)
+		if expectedProgress != maxProgress {
+			t.Errorf("Expected progress: %d, got: %d", maxProgress, expectedProgress)
+		}
+	}()
+
+	progressReporter.Start(func(p *int) {
+		if *p != expectedProgress {
+			t.Errorf("Expected progress: %d, got: %d", expectedProgress, *p)
+		}
+		expectedProgress++
+	})
+
+	for i := 0; i < maxProgress; i++ {
+		progressReporter.Update(i)
+	}
+}
+
+func TestReporter_BlockingWithRetry(t *testing.T) {
+	maxProgress := 5
+	progressReporter := NewReporter[int](1)
+	expectedProgress := 0
+
+	defer func() {
+		progressReporter.Stop(true)
+		if expectedProgress != maxProgress {
+			t.Errorf("Expected progress: %d, got: %d", maxProgress, expectedProgress)
+		}
+	}()
+
+	progressReporter.Start(func(p *int) {
+		if *p != expectedProgress {
+			t.Errorf("Expected progress: %d, got: %d", expectedProgress, *p)
+		}
+		expectedProgress++
+	})
+
+	for i := 0; i < maxProgress; i++ {
+		for {
+			if progressReporter.Update(i) {
+				break
+			}
+		}
+	}
+}
