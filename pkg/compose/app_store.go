0017a2a2e587e48a8a27b14b390bc2c3883afef6
Mike Sul <mike.sul@foundries.io>
2025-04-29 10:18:43 +0200

    app_store: Register fetched apps at app store
    If the update apps have been successfully fetched then register them at
    the app store so they are listed in the output of the `composectl ls`.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/compose/app_store.go
+++ pkg/compose/app_store.go
@@ -5,6 +5,7 @@ import "context"
 type (
 	AppStore interface {
 		BlobProvider
+		AddApps(appURIs []string) error
 		ListApps(ctx context.Context) ([]*AppRef, error)
 		RemoveApps(ctx context.Context, apps []*AppRef, prune bool) error
 		Prune(ctx context.Context) ([]string, error)
