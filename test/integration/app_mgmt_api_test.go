7559fdfa4b1abd12bee9ec3ef9985943c8b4d0f0
Mike Sul <mike.sul@foundries.io>
2025-05-14 16:22:33 +0200

    list: Add API function to list apps
    - Add the API function to list apps found in the app store.
    
    - Extend the API test to verify the list function.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- test/integration/app_mgmt_api_test.go
+++ test/integration/app_mgmt_api_test.go
@@ -1,12 +1,14 @@
 package e2e_tests
 
 import (
+	"context"
+	"github.com/foundriesio/composeapp/pkg/compose"
 	f "github.com/foundriesio/composeapp/test/fixtures"
 	"testing"
 )
 
 func TestAppApi(t *testing.T) {
-	appComposeDef := `
+	appComposeDef01 := `
 services:
   srvs-01:
     image: registry:5000/factory/runner-image:v0.1
@@ -17,12 +19,53 @@ services:
     image: ghcr.io/foundriesio/busybox:1.36
     command: sh -c "while true; do sleep 60; done"
 `
-	app := f.NewApp(t, appComposeDef)
-	app.Publish(t)
+	appComposeDef02 := `
+services:
+  srvs-01:
+    image: registry:5000/factory/runner-image:v0.1
+    command: sh -c "while true; do sleep 60; done"
+    ports:
+    - 8080:80
+  srvs-02:
+    image: registry:5000/factory/runner-image:v0.1
+    command: sh -c "while true; do sleep 60; done"
+`
+	var apps []*f.App
+	for _, appDef := range []string{appComposeDef01, appComposeDef02} {
+		app := f.NewApp(t, appDef)
+		app.Publish(t)
+		apps = append(apps, app)
+	}
+	for _, a := range apps {
+		a.Pull(t)
+	}
+	defer func() {
+		for _, a := range apps {
+			a.Remove(t)
+		}
+	}()
+	appsMap := make(map[string]bool)
+	for _, a := range apps {
+		appsMap[a.PublishedUri] = false
+	}
 
-	app.Pull(t)
-	defer app.Remove(t)
+	ctx := context.Background()
+	cfg := f.NewTestConfig(t)
 
-	f.NewTestConfig(t)
-	// TODO: extend this test to check the app management API
+	listedApps, err := compose.ListApps(ctx, cfg)
+	f.Check(t, err)
+	if len(listedApps) != len(appsMap) {
+		t.Fatalf("expected %d apps, got %d", len(appsMap), len(listedApps))
+	}
+	for _, app := range listedApps {
+		if checked, ok := appsMap[app.Ref().String()]; !ok {
+			t.Fatalf("got unexpected app: %s", app.Ref().String())
+		} else {
+			if checked {
+				t.Fatalf("app has been listed twice: %s", app.Ref().String())
+			} else {
+				appsMap[app.Ref().String()] = true
+			}
+		}
+	}
 }
