66a898d91b9879af5e6ec2ada5ce5e648c0c0419
Mike Sul <mike.sul@foundries.io>
2025-06-03 17:52:36 +0200

    fetch: Unify blob pulling WIP
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/composectl/cmd/pull.go
+++ cmd/composectl/cmd/pull.go
@@ -3,13 +3,12 @@ package composectl
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/containerd/containerd/content/local"
 	"github.com/containerd/containerd/platforms"
 	"github.com/foundriesio/composeapp/pkg/compose"
 	v1 "github.com/foundriesio/composeapp/pkg/compose/v1"
 	"github.com/spf13/cobra"
 	"os"
-	"path"
+	"strings"
 )
 
 var (
@@ -58,27 +57,60 @@ func pullApps(cmd *cobra.Command, args []string) {
 		}
 		cr.print()
 		fmt.Println("Pulling app blobs...")
-		// copying missing blobs
-		// TODO:  move to a separate function:
-		//  1) Copy in multiple goroutines/workers (configurable)
-		//  2) Generic status reporting mechanism
-		authorizer := compose.NewRegistryAuthorizer(config.DockerCfg, config.ConnectTimeout)
-		resolver := compose.NewResolver(authorizer, config.ConnectTimeout)
+		//// copying missing blobs
+		//// TODO:  move to a separate function:
+		////  1) Copy in multiple goroutines/workers (configurable)
+		////  2) Generic status reporting mechanism
+		//authorizer := compose.NewRegistryAuthorizer(config.DockerCfg, config.ConnectTimeout)
+		//resolver := compose.NewResolver(authorizer, config.ConnectTimeout)
+		//
+		//ls, err := local.NewStore(config.StoreRoot)
+		//DieNotNil(err)
+		//for _, b := range cr.MissingBlobs {
+		//	fmt.Printf(" [%-15s] %s %15d ... ", b.Type, b.Descriptor.Digest.Encoded(), b.Descriptor.Size)
+		//	var copyErr error
+		//	if len(*pullSrcStorePath) > 0 {
+		//		blobPath := path.Join(compose.GetBlobsRootFor(*pullSrcStorePath), b.Descriptor.Digest.Encoded())
+		//		copyErr = compose.CopyLocalBlob(cmd.Context(), blobPath, b.Descriptor.URLs[0], *b.Descriptor, ls, true)
+		//	} else {
+		//		copyErr = compose.CopyBlob(cmd.Context(), resolver, b.Descriptor.URLs[0], *b.Descriptor, ls, true)
+		//	}
+		//	DieNotNil(copyErr)
+		//	fmt.Println("ok")
+		//}
 
-		ls, err := local.NewStore(config.StoreRoot)
-		DieNotNil(err)
-		for _, b := range cr.MissingBlobs {
-			fmt.Printf(" [%-15s] %s %15d ... ", b.Type, b.Descriptor.Digest.Encoded(), b.Descriptor.Size)
-			var copyErr error
-			if len(*pullSrcStorePath) > 0 {
-				blobPath := path.Join(compose.GetBlobsRootFor(*pullSrcStorePath), b.Descriptor.Digest.Encoded())
-				copyErr = compose.CopyLocalBlob(cmd.Context(), blobPath, b.Descriptor.URLs[0], *b.Descriptor, ls, true)
-			} else {
-				copyErr = compose.CopyBlob(cmd.Context(), resolver, b.Descriptor.URLs[0], *b.Descriptor, ls, true)
+		var printedLines = 0
+
+		err := compose.FetchBlobs(cmd.Context(), config, cr.MissingBlobs, compose.WithFetchProgress(func(progress *compose.FetchProgress) {
+			// Move the cursor up to overwrite previous output
+			if printedLines > 0 {
+				fmt.Printf("\033[%dA", printedLines)
 			}
-			DieNotNil(copyErr)
-			fmt.Println("ok")
-		}
+
+			var sb strings.Builder
+
+			// Line 1: Overall progress
+			sb.WriteString(fmt.Sprintf("Blobs downloaded: %d/%d\n", progress.Current, progress.Total))
+
+			// Line 2: Current blob download (if any)
+			if len(progress.Blobs) > 0 {
+				blob := progress.Blobs[0]
+				var percent float64
+				if blob.Descriptor.Size > 0 {
+					percent = float64(blob.Fetched) / float64(blob.Descriptor.Size) * 100
+				}
+				sb.WriteString(fmt.Sprintf("Downloading %s: %d/%d bytes (%.1f%%)\n",
+					blob.Descriptor.Digest, blob.Fetched, blob.Descriptor.Size, percent))
+			} else {
+				sb.WriteString("Waiting for blob...\n")
+			}
+
+			// Print output and update printed line count
+			output := sb.String()
+			printedLines = strings.Count(output, "\n")
+			fmt.Print(output)
+		}))
+		DieNotNil(err, "failed to fetch blobs")
 	}
 
 	for _, app := range apps {
