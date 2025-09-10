d1d04682fab37b63f4feeaf613e31bacb0f00263
Mike Sul <mike.sul@foundries.io>
2025-09-10 12:49:56 +0200

    run: Use API functions to install and start apps
    Refactor the `composectl run` command so it uses the API function to
    install and start apps. It helps to unify overall implementation.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/composectl/cmd/run.go
+++ cmd/composectl/cmd/run.go
@@ -2,12 +2,9 @@ package composectl
 
 import (
 	"fmt"
-	"github.com/containerd/containerd/platforms"
 	"github.com/foundriesio/composeapp/pkg/compose"
-	v1 "github.com/foundriesio/composeapp/pkg/compose/v1"
+	"github.com/foundriesio/composeapp/pkg/update"
 	"github.com/spf13/cobra"
-	"os"
-	"os/exec"
 	"strings"
 )
 
@@ -48,44 +45,45 @@ func init() {
 }
 
 func runApps(cmd *cobra.Command, opts *runOptions) {
-	cs, err := v1.NewAppStore(config.StoreRoot, config.Platform)
-	DieNotNil(err)
-	apps, err := cs.ListApps(cmd.Context())
+	apps, err := compose.ListApps(cmd.Context(), config)
 	DieNotNil(err)
 
-	foundApps := map[string]bool{}
+	checkedApps := map[string]compose.App{}
 	for _, app := range apps {
-		foundApps[app.Name] = true
+		appName := app.Name()
+		if len(opts.Apps) > 0 && !opts.Apps[appName] {
+			continue
+		}
+		if _, ok := checkedApps[appName]; ok {
+			DieNotNil(fmt.Errorf("cannot start %s since there are two or more versions of it found in the store", appName))
+		}
+		checkedApps[appName] = app
 	}
 	for app := range opts.Apps {
-		if !foundApps[app] {
+		if _, ok := checkedApps[app]; !ok {
 			DieNotNil(fmt.Errorf("specified app is not present in the local store: %s", app))
 		}
 	}
 
-	checkedApps := map[string]compose.App{}
-	for _, app := range apps {
-		if len(opts.Apps) > 0 && !opts.Apps[app.Name] {
-			fmt.Printf("%s: skipping, not in the shortlist\n", app.Name)
-			continue
-		}
-		a, err := v1.NewAppLoader().LoadAppTree(cmd.Context(), cs, platforms.OnlyStrict(config.Platform), app.String())
+	var appURIs []string
+	// Make sure all apps are installed before starting any of them
+	for _, app := range checkedApps {
+		err = compose.Install(cmd.Context(), config, app.Ref().String(),
+			compose.WithInstallProgress(update.GetInstallProgressPrinter()))
 		DieNotNil(err)
-		if _, ok := checkedApps[app.Name]; ok {
-			DieNotNil(fmt.Errorf("cannot start %s since there are two or more versions of it found in the store", app.Name))
-		}
-		checkedApps[app.Name] = a
+		appURIs = append(appURIs, app.Ref().String())
 	}
 
-	for _, app := range checkedApps {
-		fmt.Printf("Installing %s --> %s\n", app.Name(), app.Ref().String())
-		err = v1.InstallApp(cmd.Context(), app, cs, config.GetBlobsRoot(), config.ComposeRoot, config.DockerHost)
-		DieNotNil(err)
-		fmt.Printf("Starting %s --> %s\n", app.Name(), app.Ref().String())
-		cmd := exec.Command("docker", "compose", "up", "-d", "--remove-orphans")
-		cmd.Dir = config.GetAppComposeDir(app.Name())
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-		DieNotNil(cmd.Run())
-	}
+	err = compose.StartApps(cmd.Context(), config, appURIs, compose.WithVerboseStart(true),
+		compose.WithStartProgressHandler(func(app compose.App, status compose.AppStartStatus, any interface{}) {
+			switch status {
+			case compose.AppStartStatusStarting:
+				fmt.Printf("Starting %s --> %s\n", app.Name(), app.Ref().String())
+			case compose.AppStartStatusStarted:
+				fmt.Printf("%s has been successfully started\n", app.Name())
+			case compose.AppStartStatusFailed:
+				fmt.Printf("failed to start %s\n", app.Name())
+			}
+		}))
+	DieNotNil(err)
 }
