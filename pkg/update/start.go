e719d5c47908b21b7f714806a0b167069dd41afa
Mike Sul <mike.sul@foundries.io>
2025-09-10 11:49:40 +0200

    start: Enhance app start API function
    - Enhance the apps start API function:
      1. Add an option to turn on/off verbose output.
      2. Add an option to specify progress callback/handler.
    
    - Use the enhanced apps start API call in the "update start"
      implementation; the goal here is to unify implementation.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/update/start.go
+++ pkg/update/start.go
@@ -3,43 +3,27 @@ package update
 import (
 	"context"
 	"fmt"
-	"github.com/containerd/containerd/platforms"
 	"github.com/foundriesio/composeapp/pkg/compose"
-	v1 "github.com/foundriesio/composeapp/pkg/compose/v1"
-	"os"
-	"os/exec"
-	"path"
 )
 
 func (u *runnerImpl) run(ctx context.Context, b *session) error {
-	cs, err := v1.NewAppStore(u.config.StoreRoot, u.config.Platform)
-	if err != nil {
+	progressStep := 100 / len(u.URIs)
+	if err := compose.StartApps(ctx, u.config, u.URIs,
+		compose.WithVerboseStart(false),
+		compose.WithStartProgressHandler(func(app compose.App, status compose.AppStartStatus, any interface{}) {
+			switch status {
+			case compose.AppStartStatusStarting:
+				fmt.Printf("\tstarting %s --> %s ... ", app.Name(), app.Ref().String())
+			case compose.AppStartStatusStarted:
+				fmt.Println("done")
+				u.Progress += progressStep
+			case compose.AppStartStatusFailed:
+				fmt.Println("failed")
+				u.Progress += progressStep
+			}
+		}),
+	); err != nil {
 		return err
 	}
-
-	apps := map[string]compose.App{}
-	for _, appURI := range u.URIs {
-		app, err := u.config.AppLoader.LoadAppTree(ctx, cs, platforms.OnlyStrict(u.config.Platform), appURI)
-		if err != nil {
-			return err
-		}
-		apps[appURI] = app
-	}
-
-	for _, app := range apps {
-		fmt.Printf("Starting %store --> %store\n", app.Name(), app.Ref().String())
-		cmd := exec.Command("docker", "compose", "up", "-d", "--remove-orphans")
-		cmd.Dir = path.Join(u.config.ComposeRoot, app.Name())
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-		err = cmd.Run()
-		if err != nil {
-			return err
-		}
-	}
-
-	u.Progress = 100
-	u.State = StateCompleted
-
 	return nil
 }
