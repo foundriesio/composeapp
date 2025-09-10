37caccc929f1a84ba65200689641a32bbcffcb52
Mike Sul <mike.sul@foundries.io>
2025-09-10 11:49:40 +0200

    start: Enhance app start API function
    - Enhance the apps start API function:
      1. Add an option to turn on/off verbose output.
      2. Add an option to specify progress callback/handler.
    
    - Use the enhanced apps start API call in the "update start"
      implementation; the goal here is to unify implementation.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/compose/start.go
+++ pkg/compose/start.go
@@ -1,13 +1,52 @@
 package compose
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/containerd/containerd/platforms"
+	"os"
 	"os/exec"
 )
 
-func StartApps(ctx context.Context, cfg *Config, appURIs []string) error {
+type (
+	StartOptions struct {
+		Verbose         bool
+		ProgressHandler AppStartProgress
+	}
+
+	StartOption func(*StartOptions)
+
+	AppStartStatus   string
+	AppStartProgress func(app App, status AppStartStatus, any interface{})
+)
+
+const (
+	AppStartStatusStarting AppStartStatus = "starting"
+	AppStartStatusStarted  AppStartStatus = "started"
+	AppStartStatusFailed   AppStartStatus = "failed"
+)
+
+func WithVerboseStart(verbose bool) StartOption {
+	return func(o *StartOptions) {
+		o.Verbose = verbose
+	}
+}
+
+func WithStartProgressHandler(handler AppStartProgress) StartOption {
+	return func(o *StartOptions) {
+		o.ProgressHandler = handler
+	}
+}
+
+func StartApps(ctx context.Context, cfg *Config, appURIs []string, options ...StartOption) error {
+	opts := &StartOptions{
+		Verbose: false,
+	}
+	for _, o := range options {
+		o(opts)
+	}
+
 	cs, err := cfg.AppStoreFactory()
 	if err != nil {
 		return err
@@ -23,14 +62,34 @@ func StartApps(ctx context.Context, cfg *Config, appURIs []string) error {
 	}
 
 	for _, app := range apps {
-		// TODO: use the docker compose API to start apps
+		if opts.ProgressHandler != nil {
+			opts.ProgressHandler(app, AppStartStatusStarting, nil)
+		}
 		cmd := exec.Command("docker", "compose", "up", "-d", "--remove-orphans")
 		cmd.Dir = cfg.GetAppComposeDir(app.Name())
-		if _, err := cmd.CombinedOutput(); err != nil {
-			if exitErr, ok := err.(*exec.ExitError); ok {
-				return fmt.Errorf("failed to start %s: %s; %s", app, exitErr.Error(), string(exitErr.Stderr))
+		if opts.Verbose {
+			// Directly connect to stdout/stderr, so we can see the output in real time
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+		} else {
+			// Capture stdout/stderr for error reporting
+			var stdout, stderr bytes.Buffer
+			cmd.Stdout = &stdout
+			cmd.Stderr = &stderr
+		}
+		if err := cmd.Run(); err != nil {
+			if opts.ProgressHandler != nil {
+				opts.ProgressHandler(app, AppStartStatusFailed, err)
 			}
-			return err
+			if opts.Verbose {
+				return fmt.Errorf("failed to start %s: %w", app, err)
+			} else {
+				return fmt.Errorf("failed to start %s: %w\n\tstdout: %s\n\tstderr: %s", app, err, cmd.Stdout, cmd.Stderr)
+			}
+
+		}
+		if opts.ProgressHandler != nil {
+			opts.ProgressHandler(app, AppStartStatusStarted, nil)
 		}
 	}
 	return nil
