2875e14ff71573b96ce661b44a3332aaae91c9a1
Mike Sul <mike.sul@foundries.io>
2026-02-02 15:34:14 +0100

    refact(run): allow specifying app URIs
    - Allow specifying apps to run via URIs in addition to names.
    
    - Use `StringSliceVar` for the `--apps` flag to leverage the cobra
      builtin functionality and simplify implementation.
    
    - Do better checking if the same app is specified more than once through
      its name and/or URI.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/composectl/cmd/run.go
+++ cmd/composectl/cmd/run.go
@@ -2,15 +2,15 @@ package composectl
 
 import (
 	"fmt"
+
 	"github.com/foundriesio/composeapp/pkg/compose"
 	"github.com/foundriesio/composeapp/pkg/update"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 type (
 	runOptions struct {
-		Apps map[string]bool
+		Apps []string
 	}
 )
 
@@ -21,60 +21,31 @@ func init() {
 		Long:  ``,
 		Args:  cobra.ArbitraryArgs,
 	}
-	opts := runOptions{Apps: map[string]bool{}}
-	runAppShortlist := runCmd.Flags().String("apps", ",", "Comma separated list of apps to run;"+
-		" all installed apps are started if not defined")
+	opts := runOptions{}
+	runCmd.Flags().StringSliceVar(&opts.Apps, "apps", nil, "Comma-separated list of apps to run")
 
 	runCmd.Run = func(cmd *cobra.Command, args []string) {
-		if *runAppShortlist == "," && len(args) == 0 {
-			DieNotNil(fmt.Errorf("at least one app must be specified as an argument or in `--apps` parameter"))
+		if len(opts.Apps) > 0 && len(args) > 0 {
+			DieNotNil(fmt.Errorf("cannot use both app list in `--apps` parameter and as arguments"))
 		}
-		if len(*runAppShortlist) > 0 && *runAppShortlist != "," {
-			for _, a := range strings.Split(*runAppShortlist, ",") {
-				opts.Apps[a] = true
-			}
-		} else {
-			for _, a := range args {
-				opts.Apps[a] = true
-			}
+		if len(args) > 0 {
+			opts.Apps = args
 		}
-
 		runApps(cmd, &opts)
 	}
 	rootCmd.AddCommand(runCmd)
 }
 
 func runApps(cmd *cobra.Command, opts *runOptions) {
-	apps, err := compose.ListApps(cmd.Context(), config)
-	DieNotNil(err)
+	appURIs := checkUserListedApps(cmd.Context(), config, opts.Apps)
 
-	checkedApps := map[string]compose.App{}
-	for _, app := range apps {
-		appName := app.Name()
-		if len(opts.Apps) > 0 && !opts.Apps[appName] {
-			continue
-		}
-		if _, ok := checkedApps[appName]; ok {
-			DieNotNil(fmt.Errorf("cannot start %s since there are two or more versions of it found in the store", appName))
-		}
-		checkedApps[appName] = app
-	}
-	for app := range opts.Apps {
-		if _, ok := checkedApps[app]; !ok {
-			DieNotNil(fmt.Errorf("specified app is not present in the local store: %s", app))
-		}
-	}
-
-	var appURIs []string
 	// Make sure all apps are installed before starting any of them
-	for _, app := range checkedApps {
-		err = compose.Install(cmd.Context(), config, app.Ref().String(),
-			compose.WithInstallProgress(update.GetInstallProgressPrinter()))
-		DieNotNil(err)
-		appURIs = append(appURIs, app.Ref().String())
+	for _, appURI := range appURIs {
+		DieNotNil(compose.Install(cmd.Context(), config, appURI,
+			compose.WithInstallProgress(update.GetInstallProgressPrinter())))
 	}
 
-	err = compose.StartApps(cmd.Context(), config, appURIs, compose.WithVerboseStart(true),
+	DieNotNil(compose.StartApps(cmd.Context(), config, appURIs, compose.WithVerboseStart(true),
 		compose.WithStartProgressHandler(func(app compose.App, status compose.AppStartStatus, any interface{}) {
 			switch status {
 			case compose.AppStartStatusStarting:
@@ -84,6 +55,5 @@ func runApps(cmd *cobra.Command, opts *runOptions) {
 			case compose.AppStartStatusFailed:
 				fmt.Printf("failed to start %s\n", app.Name())
 			}
-		}))
-	DieNotNil(err)
+		})))
 }
