979c33b8c8c6ef7f6367568ee09fad36a8d97ca3
Mike Sul <mike.sul@foundries.io>
2026-04-23 17:35:54 +0200

    compose: fix app image pruning and add all-unused-images mode
    Fix image pruning in only-app-images mode.
    
    The previous implementation used the Docker image prune API with
    dangling=true, which only removes untagged dangling images and did not
    prune unused images that were still referenced by tags or digests.
    
    Uninstall now explicitly detects images not used by any container and,
    in only-app-images mode, removes only those images related to the apps
    being uninstalled.
    
    Add a new all-unused-images mode that removes every image not used by
    any container, regardless of whether it is related to compose apps.
    
    This makes uninstall cleanup more accurate for app-specific pruning
    while providing an optional broader system cleanup mode.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/compose/uninstall.go
+++ pkg/compose/uninstall.go
@@ -3,8 +3,14 @@ package compose
 import (
 	"context"
 	"errors"
-	"github.com/docker/docker/api/types/filters"
+	"fmt"
 	"os"
+
+	"github.com/containerd/containerd/reference/docker"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 )
 
 type (
@@ -18,7 +24,8 @@ type (
 )
 
 var (
-	ErrUninstallRunningApps            = errors.New("failed to uninstall apps: some apps are still running, please stop them first")
+	ErrUninstallRunningApps = errors.New("failed to uninstall apps: some apps are still running, please stop them first")
+
 	PruneTypeAllUnusedImages PruneType = "all-unused-images"
 	PruneTypeOnlyAppImages   PruneType = "only-app-images"
 )
@@ -72,8 +79,7 @@ func UninstallApps(ctx context.Context, cfg *Config, appRefs []string, options .
 				continue
 			}
 		}
-		err = os.RemoveAll(cfg.GetAppComposeDir(app.Name()))
-		if err != nil {
+		if err = os.RemoveAll(cfg.GetAppComposeDir(app.Name())); err != nil {
 			return err
 		}
 	}
@@ -81,21 +87,98 @@ func UninstallApps(ctx context.Context, cfg *Config, appRefs []string, options .
 	if opts.Prune {
 		cli, errClient := GetDockerClient(cfg.DockerHost)
 		if errClient != nil {
-			return errClient
+			return fmt.Errorf("failed to create docker client: %w", errClient)
 		}
-		// Prune only dangling images.
-		// The dangling images are the ones that are not tagged and not referenced by any container.
-		// TODO: consider pruning volumes and networks if needed.
-		// TODO: consider pruning only those images that are related to the uninstalled apps,
-		//       otherwise it prunes all dangling images including those that are not managed by composectl
-		var dangling string
+
+		var err error
+		var allImages []image.Summary
+		if allImages, err = cli.ImageList(ctx, types.ImageListOptions{All: true}); err != nil {
+			return fmt.Errorf("failed to list images: %w", err)
+		}
+
+		imagesNotInUse := make(map[string]image.Summary)
+		for _, img := range allImages {
+			imagesNotInUse[img.ID] = img
+		}
+
+		var allContainers []types.Container
+		if allContainers, err = cli.ContainerList(ctx, container.ListOptions{All: true}); err != nil {
+			return fmt.Errorf("failed to list containers: %w", err)
+		}
+		for _, ctr := range allContainers {
+			delete(imagesNotInUse, ctr.ImageID)
+		}
+
 		switch opts.PruneType {
 		case PruneTypeAllUnusedImages:
-			dangling = "false"
+			// Remove all images that are not in use by any container.
+			for imgID := range imagesNotInUse {
+				// TODO: print debug message about which image is being removed and any error that occurs during removal.
+				_, _ = cli.ImageRemove(ctx, imgID, types.ImageRemoveOptions{Force: true, PruneChildren: true})
+			}
 		case PruneTypeOnlyAppImages:
-			dangling = "true"
+			// Build a map of image refs to image summary for images that are not in use by any container.
+			// We will use this map to check if an image ref related to the uninstalled apps is used by any container before removing it.
+			imageRefsNotInUse := make(map[string]image.Summary)
+			setAllImageRefVariants := func(ref string) {
+				imageRefsNotInUse[ref] = imagesNotInUse[ref]
+				// Make sure to consider all variants of the same image ref, "normalized" and "familiar"
+				if anyRef, err := docker.ParseAnyReference(ref); err == nil {
+					imageRefsNotInUse[anyRef.String()] = imagesNotInUse[ref]
+					if familiarRef := docker.FamiliarString(anyRef); len(familiarRef) > 0 {
+						imageRefsNotInUse[familiarRef] = imagesNotInUse[ref]
+					}
+				}
+			}
+			for _, img := range imagesNotInUse {
+				for _, ref := range img.RepoDigests {
+					setAllImageRefVariants(ref)
+				}
+				for _, ref := range img.RepoTags {
+					setAllImageRefVariants(ref)
+				}
+			}
+			// Remove images that are referenced by the apps being uninstalled and are not in use by any container.
+			removeAppImageRefs(ctx, status.Apps, cli, imageRefsNotInUse)
 		}
-		_, err = cli.ImagesPrune(ctx, filters.NewArgs(filters.Arg("dangling", dangling)))
 	}
 	return err
+}
+
+func removeAppImageRefs(ctx context.Context, apps []App, cli *client.Client, imageRefsNotInUse map[string]image.Summary) {
+	// Collect image refs related to the uninstalled apps.
+	var imageRefsToPrune []string
+	for _, app := range apps {
+		for _, imageRoot := range app.GetComposeRoot().Children {
+			curImageRoot := imageRoot
+			for {
+				imageRef := curImageRoot.Ref()
+				// Add a digest ref
+				imageRefsToPrune = append(imageRefsToPrune, imageRef)
+				if ref, err := ParseImageRef(imageRef); err == nil {
+					// Add a tag ref
+					imageRefsToPrune = append(imageRefsToPrune, ref.GetTagRef())
+				}
+				if curImageRoot.Type == BlobTypeImageManifest || len(curImageRoot.Children) == 0 {
+					break
+				}
+				// the image root points to an image index, let's add refs that point to the image manifest
+				curImageRoot = curImageRoot.Children[0]
+			}
+		}
+	}
+	// Remove image refs related to the uninstalled apps and images the refs point to are not in use by any container.
+	// If the removed ref is the only ref for the image, the image will also be removed;
+	// if there are other refs for the image, only the removed ref will be removed.
+	// This is the best effort to remove images related to the uninstalled apps without
+	// affecting other apps that may share the same images.
+	// In some case it can remove an image for which there is no container but some other utility reference it
+	// by the same reference as the uninstalled app, but that is an acceptable edge case and best effort
+	// to clean up images related to the uninstalled apps.
+	for _, ref := range imageRefsToPrune {
+		if _, notInUse := imageRefsNotInUse[ref]; notInUse {
+			// TODO: print debug message about which image is being removed and any error that occurs during removal.
+			_, _ = cli.ImageRemove(ctx, ref, types.ImageRemoveOptions{Force: false, PruneChildren: true})
+		}
+	}
 }
